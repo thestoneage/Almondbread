ef90ea3e025da32bd80d3778481caa97e4d89256
Jörg W Mittag <JoergWMittag+Almondbread@GoogleMail.Com>
2010-07-30 00:33:28 +0200

    Use more idiomatic notation for impure methods.

--- Mandelbrot.scala
+++ Mandelbrot.scala
@@ -4,7 +4,7 @@ import scala.collection.immutable.Range.Double
 
 object Mandelbrot {
 
-  def main(args: Array[String]): Unit = {
+  def main(args: Array[String]) {
     
     each_point(80, 20, (x:Int, y:Int, v:Int) => {
         if (x==0) println()
@@ -22,7 +22,7 @@ object Mandelbrot {
     return step
   }
   
-  def printMandelSet():Unit = {
+  def printMandelSet() {
     Range.Double(-1.0, 1.0, 2.0/20.0).foreach(i => {
       Range.Double(-2.0, 1.0, 3.0/80.0).foreach(r => 
         print(if (escapeTime(new Complex(r,i)) < 255) '-' else '*'))
@@ -30,7 +30,7 @@ object Mandelbrot {
       })
   }
   // More Functional approach
-  def each_point(width:Int, height:Int, func: (Int, Int, Int) => Unit):Unit = {
+  def each_point(width:Int, height:Int, func: (Int, Int, Int) => Unit) {
     val rs = 3.0/width
     val is = 2.0/height
     Range.Int(0, height, 1).map(y => (-1.0 + is * y, y)).foreach((i) => {
@@ -41,7 +41,7 @@ object Mandelbrot {
   }
 
   // More Procedural
-  def foreach_point(width:Int, height:Int, func: (Int, Int, Int) => Unit):Unit = {
+  def foreach_point(width:Int, height:Int, func: (Int, Int, Int) => Unit) {
     var r = -2.0
     var i = -1.0
     Range.Int(0, height, 1).foreach((y:Int) => {
