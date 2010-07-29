9b7914b0aa3e053f8ab49992d05dba892c2115f1
Jörg W Mittag <JoergWMittag+Almondbread@GoogleMail.Com>
2010-07-30 00:49:15 +0200

    Use for comprehensions.

--- Mandelbrot.scala
+++ Mandelbrot.scala
@@ -23,35 +23,33 @@ object Mandelbrot {
   }
   
   def printMandelSet {
-    Range.Double(-1.0, 1.0, 2.0/20.0).foreach(i => {
-      Range.Double(-2.0, 1.0, 3.0/80.0).foreach(r => 
-        print(if (escapeTime(new Complex(r,i)) < 255) '-' else '*'))
+    for (i <- Range.Double(-1.0, 1.0, 2.0/20.0)) {
+      for (r <- Range.Double(-2.0, 1.0, 3.0/80.0))
+        print(if (escapeTime(new Complex(r,i)) < 255) '-' else '*')
       println
-    })
+    }
   }
   // More Functional approach
   def each_point(width:Int, height:Int, func: (Int, Int, Int) => Unit) {
     val rs = 3.0/width
     val is = 2.0/height
-    Range.Int(0, height, 1).map(y => (-1.0 + is * y, y)).foreach((i) => {
-        Range.Int(0, width, 1).map(x => (-2.0 + rs * x, x)).foreach((r) => {
+    for (i <- Range.Int(0, height, 1).map(y => (-1.0 + is * y, y)))
+        for (r <- Range.Int(0, width, 1).map(x => (-2.0 + rs * x, x)))
             func(r._2, i._2, escapeTime(new Complex(r._1, i._1)))
-      })
-    })
   }
 
   // More Procedural
   def foreach_point(width:Int, height:Int, func: (Int, Int, Int) => Unit) {
     var r = -2.0
     var i = -1.0
-    Range.Int(0, height, 1).foreach((y:Int) => {
-        Range.Int(0, width, 1).foreach((x:Int) => {
+    for (y <- Range.Int(0, height, 1)) {
+        for (x <- Range.Int(0, width, 1)) {
             func(x, y, escapeTime(new Complex(r, i)))
             r += (3.0/width)
-      })
+        }
       r = -2.0
       i += (2.0/height)
-    })
+    }
   }  
 
 
