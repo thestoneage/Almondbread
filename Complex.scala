3c198b6fab0193e52d5843e69ce1873734001b7f
Jörg W Mittag <JoergWMittag+Almondbread@GoogleMail.Com>
2010-07-30 01:13:24 +0200

    Funnily enough, Scala provides a factory for us.

--- Complex.scala
+++ Complex.scala
@@ -1,13 +1,9 @@
 package sandelbrot
 
-class Complex (val real: Double, val imag: Double){
+case class Complex (val real: Double, val imag: Double){
   def +(that: Complex) = Complex(real + that.real, imag + that.imag)
   def +(that: Double) = Complex(real + that, imag)
   def *(that: Complex) = Complex(real * that.real - imag * that.imag,
         imag*that.real + real * that.imag)
   def abs = scala.math.hypot(real, imag)
-}
-
-object Complex {
-  def apply(real: Double, imag: Double) = new Complex(real, imag)
 }
