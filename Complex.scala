ff2d0ac75f74f5b14b698a23e8e24d4d40305693
Jörg W Mittag <JoergWMittag+Almondbread@GoogleMail.Com>
2010-07-30 01:15:46 +0200

    Since Complex is immutable, abs doesn't need to be a method.
    Its value can't change anyway.

--- Complex.scala
+++ Complex.scala
@@ -5,5 +5,5 @@ case class Complex (protected val real: Double, protected val imag: Double){
   def +(that: Double) = Complex(real + that, imag)
   def *(that: Complex) = Complex(real * that.real - imag * that.imag,
         imag*that.real + real * that.imag)
-  def abs = scala.math.hypot(real, imag)
+  val abs = scala.math.hypot(real, imag)
 }
