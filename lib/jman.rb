3a13ed3a933f076b3f1c6acb3e236b9bdc00cf39
Jörg W Mittag <JoergWMittag+Almondbread@GoogleMail.Com>
2010-08-01 04:24:26 +0200

    Use the automatic code formatter for consistency's sake.

--- lib/jman.rb
+++ lib/jman.rb
@@ -5,18 +5,18 @@ include_class javax.swing.JFrame
 include_class java.awt.image.BufferedImage
 
 class ImagePanel < javax.swing.JPanel
-    def initialize(image)
-        super()
-        @image = image
-    end
+  def initialize(image)
+    super()
+    @image = image
+  end
 
-    def paint(g)
-        g.draw_image(@image, 0, 0, self)
-    end
+  def paint(g)
+    g.draw_image(@image, 0, 0, self)
+  end
 
-    def getPreferredSize()
-        java.awt.Dimension.new(@image.width, @image.height)
-    end
+  def getPreferredSize()
+    java.awt.Dimension.new(@image.width, @image.height)
+  end
 end
 
 image = BufferedImage.new(640, 480, BufferedImage::TYPE_INT_RGB)
@@ -25,9 +25,7 @@ frame.content_pane.add(ImagePanel.new(image))
 frame.pack
 frame.show(true)
 
-
 Mandelbrot.each_point(ComplexRange.new((-2.0..0.5), (-1.0..1.0)), Resolution.new(image.width, image.height)) { |value, x, y|
-    image.setRGB(x, y, value + (value << 8) + (value << 16))
-    frame.repaint
+  image.setRGB(x, y, value + (value << 8) + (value << 16))
+  frame.repaint
 }
-
--- lib/rman.rb
+++ lib/rman.rb
@@ -1,59 +1,58 @@
 require 'complex' unless defined? Complex
 
 class << (Mandelbrot = Object.new)
-    def escape_time(c)
-        z, step = Complex(0, 0), 0
-        while step < 256 && z.abs < 2
-            z = z * z + c
-            step += 1
-        end
-
-        return step
+  def escape_time(c)
+    z, step = Complex(0, 0), 0
+    while step < 256 && z.abs < 2
+      z = z * z + c
+      step += 1
     end
 
-    #Print a mandelbrot set to stdout
-    def print_set(width = 80, height = 24)
-        (-1.0..1.0).step(2.0/(height - 1)).each do |y|
-            (-2.0..0.5).step(2.5/(width - 1)) do |x|
-                print escape_time(Complex(x, y)) < 255 ? '*' : ' '
-            end
-            puts
-        end
-    end
+    return step
+  end
 
-    def each_point(crange, resolution)
-        r, i = crange.real.first, crange.imaginary.first
-        step_r, step_i = crange.real.length / resolution.width, crange.imaginary.length / resolution.height
-        resolution.height.times do |y|
-            resolution.width.times do |x|
-                yield escape_time(Complex(r, i)), x, y
-                r += step_r
-            end
-            r = crange.real.first
-            i += step_i
-        end
+  #Print a mandelbrot set to stdout
+  def print_set(width = 80, height = 24)
+    (-1.0..1.0).step(2.0/(height - 1)).each do |y|
+      (-2.0..0.5).step(2.5/(width - 1)) do |x|
+        print escape_time(Complex(x, y)) < 255 ? '*' : ' '
+      end
+      puts
     end
+  end
 
+  def each_point(crange, resolution)
+    r, i = crange.real.first, crange.imaginary.first
+    step_r, step_i = crange.real.length / resolution.width, crange.imaginary.length / resolution.height
+    resolution.height.times do |y|
+      resolution.width.times do |x|
+        yield escape_time(Complex(r, i)), x, y
+        r += step_r
+      end
+      r = crange.real.first
+      i += step_i
+    end
+  end
 end
 
 class ComplexRange
-    attr_reader :real, :imaginary
+  attr_reader :real, :imaginary
 
-    def initialize(real, imaginary)
-        @real, @imaginary = real, imaginary
-    end
+  def initialize(real, imaginary)
+    @real, @imaginary = real, imaginary
+  end
 end
 
 class Range
-    def length
-        last - first
-    end
+  def length
+    last - first
+  end
 end
 
 class Resolution
-    attr_reader :width, :height
+  attr_reader :width, :height
 
-    def initialize(width, height)
-        @width, @height = width, height
-    end
+  def initialize(width, height)
+    @width, @height = width, height
+  end
 end
