8152af3125f823ffb1c78ac936b022927d45cf4f
Jörg W Mittag <JoergWMittag+Almondbread@GoogleMail.Com>
2010-08-01 04:22:45 +0200

    Rubify. Don't let Headius's hard work go to waste!

--- lib/jman.rb
+++ lib/jman.rb
@@ -11,17 +11,17 @@ class ImagePanel < javax.swing.JPanel
     end
 
     def paint(g)
-        g.drawImage(@image, 0, 0, self)
+        g.draw_image(@image, 0, 0, self)
     end
 
     def getPreferredSize()
-        java.awt.Dimension.new(@image.getWidth(), @image.getHeight())
+        java.awt.Dimension.new(@image.width, @image.height)
     end
 end
 
 image = BufferedImage.new(640, 480, BufferedImage::TYPE_INT_RGB)
 frame = JFrame.new("Mandelbrot")
-frame.getContentPane().add(ImagePanel.new(image))
+frame.content_pane.add(ImagePanel.new(image))
 frame.pack
 frame.show(true)
 
