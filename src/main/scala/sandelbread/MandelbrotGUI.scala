c6151320f2b3eed73b1ecc6fef3bd0fe448e418e
Jörg W Mittag <JoergWMittag+Almondbread@GoogleMail.Com>
2010-08-01 03:11:27 +0200

    Use the automatic code formatter for consistency's sake.

--- src/main/scala/sandelbread/MandelbrotGUI.scala
+++ src/main/scala/sandelbread/MandelbrotGUI.scala
@@ -3,7 +3,6 @@ package sandelbread
 import scala.swing._
 import event._
 
-
 object MandelbrotGUI extends scala.swing.SimpleSwingApplication {
   private val width = 640
   private val height = 480
@@ -14,7 +13,7 @@ object MandelbrotGUI extends scala.swing.SimpleSwingApplication {
     title = "Mandelbrot"
     val button = new Button {
       text = "Start"
-    } 
+    }
     val panel = new Panel {
       override def paint(g:scala.swing.Graphics2D) = {
         g.drawImage(image, 0, 0, null)
@@ -29,10 +28,9 @@ object MandelbrotGUI extends scala.swing.SimpleSwingApplication {
     reactions += {
       case ButtonClicked(b) =>
         Mandelbrot.eachPoint(width, height, (x, y, v) => {
-          image.setRGB(x, y, v + (v << 8) + (v << 16))
-        })
+            image.setRGB(x, y, v + (v << 8) + (v << 16))
+          })
         panel.repaint
-        
     }
   }
 }
