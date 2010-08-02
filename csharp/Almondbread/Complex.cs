06153c807287ee64a9bc6a7af04cf54e67a61ce2
Jörg W Mittag <JoergWMittag+Almondbread@GoogleMail.Com>
2010-08-02 15:56:07 +0200

    And C#. A little nicer than Java, thanks to operator overloading and lambdas.

---
+++ csharp/Almondbread/Complex.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Almondbread
+{
+    internal sealed class Complex
+    {
+        internal static Complex ZERO = new Complex();
+        private double real;
+        private double imag;
+        private double abs;
+
+        internal Complex(double real = 0, double imag = 0)
+        {
+            this.real = real;
+            this.imag = imag;
+            this.abs = Math.Sqrt(real * real + imag * imag);
+        }
+
+        public static Complex operator +(Complex a, Complex b)
+        {
+            return new Complex(a.real + b.real, a.imag + b.imag);
+        }
+
+        public static Complex operator +(Complex a, double b)
+        {
+            return new Complex(a.real + b, a.imag);
+        }
+
+        public static Complex operator +(double b, Complex a)
+        {
+            return new Complex(a.real + b, a.imag);
+        }
+
+        public static Complex operator *(Complex a, Complex b)
+        {
+            return new Complex(a.real * b.real - a.imag * b.imag,
+                    a.imag * b.real + a.real * b.imag);
+        }
+        public static implicit operator double(Complex c)
+        {
+            return c.abs;
+        }
+    }
+}
---
+++ csharp/Almondbread/Mandelbrot.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace Almondbread
+{
+    internal sealed class Mandelbrot
+    {
+        private const int width = 80;
+        private const int height = 20;
+
+        private static void Main(string[] args)
+        {
+            EachPoint(width, height, (x, y, v) =>
+            {
+                if (x == 0)
+                {
+                    Console.WriteLine();
+                }
+                Console.Write(v < 255 ? "-" : "#");
+            });
+            Console.WriteLine();
+            PrintMandelSet();
+        }
+
+        private static short EscapeTime(Complex c)
+        {
+            short step = 0;
+            var z = Complex.ZERO;
+            while (step++ < 256 && z < 4)
+            {
+                z = z * z + c;
+            }
+            return step;
+        }
+
+        private static short EscapeTime2(Complex c)
+        {
+            return EscapeTime2Recursive(c, Complex.ZERO);
+        }
+
+        private static short EscapeTime2Recursive(Complex c, Complex z, short step = 0)
+        {
+            if (step > 256 || z > 4)
+            {
+                return step;
+            }
+            return EscapeTime2Recursive(c, z * z + c, (short)(step + 1));
+        }
+
+        private static void PrintMandelSet(int width = width, int height = height)
+        {
+            for (double i = -1.0; i < 1.0; i += 2.0 / height)
+            {
+                for (double r = -2.0; r < 0.5; r += 2.5 / width)
+                {
+                    Console.Write(EscapeTime2(new Complex(r, i)) < 255 ? "*" : " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        internal static void EachPoint(int width, int height, Action<int, int, short> func)
+        {
+            var r = -2.0;
+            var i = -1.0;
+            for (var y = 0; y < height; y++)
+            {
+                for (var x = 0; x < width; x++)
+                {
+                    func(x, y, EscapeTime2(new Complex(r, i)));
+                    r += 3.0 / width;
+                }
+                r = -2.0;
+                i += 2.0 / height;
+            }
+        }
+    }
+}
