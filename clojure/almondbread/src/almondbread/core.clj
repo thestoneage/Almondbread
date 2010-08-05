089850ef51e55bd6a47483be801477718be624be
Marc Rummel <marc.rummel@gmail.com>
2010-08-05 12:17:07 +0200

    Works using Lists/Maps

--- clojure/almondbread/src/almondbread/core.clj
+++ clojure/almondbread/src/almondbread/core.clj
@@ -6,26 +6,22 @@
             [clojure.contrib.complex-numbers :as cn]))
 
 (defn escape-time
-  [c z step]
+  ([c]
+  (escape-time c (cn/complex 0 0) 0))
+  ([c z step]
   (if (or (> step 256) (gc/> (gm/abs z) 4.0))
     step
-    (recur c (ga/+(ga/* z z) c) (+ step 1))))
-
-(defn escape-time-single
-  [c] (escape-time c (cn/complex 0 0) 0))
+    (recur c (ga/+(ga/* z z) c) (+ step 1)))))
 
 (defn each-point
-  [width height f]
+  [width height]
   (let [rs (/ 3.0 width)
         is (/ 3.0 height)]
     (for [y (range height) x (range width)]
-      (f x y (escape-time-single
-               (cn/complex (+ -2.0 (* rs x)) (+ -1.0 (* is y))))))))
+      {:x x, :y y, :v (escape-time
+               (cn/complex (+ -2.0 (* rs x)) (+ -1.0 (* is y))))})))
 
-(defn print-mandelset [width height]
-  (each-point width height (fn [x y v]
-                      (if (= 0 x)
-                        (println ""))
-                      (if (< v 255) (print "-") (print "#")))))
+(defn mandelstr [width height] (map (fn [i] (.concat (if (= (:x i) 0) "\n" "") (if (< (:v i) 255) "-" "#"))) (each-point width height)))
 
-(defn -main [& args] (print-mandelset))
+(println (apply str (interpose "" (mandelstr 80 24))))
+
